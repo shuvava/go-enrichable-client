b3147bd2830c5e303d511fe36854d57a24edab71
shuvava <shuvava@users.noreply.github.com>
2021-07-25 08:59:44 -0700

    add simple client example

---
+++ examples/simple-client/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/shuvava/go-enrichable-client/client"
+)
+
+// User API user data model
+type User struct {
+	ID        int    `json:"id"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+// Response API response data model
+type Response struct {
+	Data []User `json:"data"`
+}
+
+func prettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
+func main() {
+	url := "https://reqres.in/api/users"
+	c := client.DefaultClient()
+	resp, err := c.Get(url)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	var responseObject Response
+	json.Unmarshal(bodyBytes, &responseObject)
+	s := prettyPrint(responseObject)
+	fmt.Printf("API Response as struct %s\n", s)
+	// fmt.Printf("API Response as struct %+v\n", responseObject)
+}
